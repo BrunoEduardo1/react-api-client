c63844703977d02c0942d46b31157a68649d55fa
BrunoEduardo1 <brunoeduardocoe@gmail.com>
2018-12-18 12:25:50 -0200

    Updates

---
+++ precache-manifest.9aa81d1b2175cbd4f5638706d602bec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "618977302c7422bc331f",
+    "url": "/react-api-client/static/js/runtime~main.61897730.js"
+  },
+  {
+    "revision": "086bee6b16f6c2ed89f3",
+    "url": "/react-api-client/static/js/main.086bee6b.chunk.js"
+  },
+  {
+    "revision": "8b6d95e1ff7701d530a7",
+    "url": "/react-api-client/static/js/1.8b6d95e1.chunk.js"
+  },
+  {
+    "revision": "086bee6b16f6c2ed89f3",
+    "url": "/react-api-client/static/css/main.3b99c65e.chunk.css"
+  },
+  {
+    "revision": "d2ec2f439717a3e6393312478d71df88",
+    "url": "/react-api-client/index.html"
+  }
+];
